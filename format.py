4ac0e47a9a46990eab2a92377f46c9fb816ea4fe
George Field <gfield145@gmail.com>
2020-07-07 23:41:08 +0100

    Add argument parsing and add option to output to stdout.

--- format.py
+++ format.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import sys
 
 from antlr4 import *
@@ -7,18 +8,28 @@ from antlr4 import *
 from perparse import PERLexer, PERParser
 from FormattedPERListener import FormattedPERListener
 
-def main(argv):
-    inp = FileStream(argv[1])
+def main():
+    parser = argparse.ArgumentParser(description = 'Formatter for AoE2 .per files')
+    parser.add_argument('inpath', type=str, help='The path to the .per file to format')
+    parser.add_argument('out', type=str, help='The path to write output to, or stdout if -')
+    args = parser.parse_args()
+
+    if args.out == '-':
+        format(args.inpath, sys.stdout)
+    else:
+        with open(args.out, "w", encoding="utf8", newline="\r\n") as output:
+            format(args.inpath, output)
+
+def format(in_path, out_stream):
+    inp = FileStream(in_path)
     lexer = PERLexer(inp)
     stream = CommonTokenStream(lexer)
     parser = PERParser(stream)
     tree = parser.per()
 
-    output = open("out.per", "w", encoding="utf8", newline="\r\n")
-
-    formatter = FormattedPERListener(output)
+    formatter = FormattedPERListener(out_stream)
     walker = ParseTreeWalker()
     walker.walk(formatter, tree)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
