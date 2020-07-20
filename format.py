1bc08edc6ec5613b1e5a4ff526fac98262778e55
George Field <gfield145@gmail.com>
2020-07-20 22:41:54 +0100

    Take keyword args to format_per and allow input from string or stdin.
    Also enables read from stdin in format.py.
    
    Sadly we can't use a standard library Python stream, as
    antlr4-python3-runtime instead implements it's own stream class. Instead
    of buffering to a string ourselves, instead we rely on the provided
    InputStream implementations for compatibility + maintainability reasons.

--- format.py
+++ format.py
@@ -6,6 +6,9 @@ Usage: format.py <Input Path> <Output Path>
 Formats an Age of Empires 2 AI rule file (.per), into a standardised
 human readable format. Comments are preserved.
 
+Input path may be '-', in which case the .per content will be read from
+stdin.
+
 Output path may be '-', in which case the formatted .per content will
 be output on stdout.
 
@@ -23,15 +26,22 @@ from performat import format_per
 def main():
     """Main script function, providing a simple CLI for the formatter."""
     parser = argparse.ArgumentParser(description='Formatter for AoE2 .per files')
-    parser.add_argument('inpath', type=str, help='The path to the .per file to format')
+    parser.add_argument('src', type=str, help='The path to the .per file to format, or stdin if -')
     parser.add_argument('out', type=str, help='The path to write output to, or stdout if -')
     args = parser.parse_args()
 
+    format_args = {}
+
+    if args.src == '-':
+        format_args['in_stdin'] = True
+    else:
+        format_args['in_path'] = args.src
+
     if args.out == '-':
-        format_per(args.inpath, sys.stdout)
+        format_per(out_stream=sys.stdout, **format_args)
     else:
         with open(args.out, "w", encoding="utf8", newline="\r\n") as output:
-            format_per(args.inpath, output)
+            format_per(out_stream=output, **format_args)
 
 if __name__ == "__main__":
     main()
--- performat/formatter.py
+++ performat/formatter.py
@@ -2,15 +2,26 @@
 Provides a formatter for AoE2 AI rule files (.per)
 """
 
-from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
+from antlr4 import FileStream, InputStream, StdinStream, CommonTokenStream, ParseTreeWalker
 
 from .perparse import PERLexer, PERParser
 from .formatted_per_listener import FormattedPERListener
 
-def format_per(in_path, out_stream):
+def format_per(*, in_path=None, in_string=None, in_stdin=False, out_stream):
     """Parses and formats a .per file, writing formatted output to the supplied text stream."""
-    inp = FileStream(in_path)
-    lexer = PERLexer(inp)
+    if [in_path is not None, in_string is not None, in_stdin].count(True) > 1:
+        raise ValueError("Only one of in_path or in_stream must be provided.")
+
+    if in_path is not None:
+        antlr_stream = FileStream(in_path)
+    elif in_string is not None:
+        antlr_stream = InputStream(in_string)
+    elif in_stdin:
+        antlr_stream = StdinStream()
+    else:
+        raise ValueError("Either in_path, in_string, or in_stdin must be provided.")
+
+    lexer = PERLexer(antlr_stream)
     stream = CommonTokenStream(lexer)
     parser = PERParser(stream)
     tree = parser.per()
